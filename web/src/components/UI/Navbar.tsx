aa4325a84a9c70043f748b8925fca3c91239cf61
Iván <ivantbd87@gmail.com>
2023-06-05 09:49:29 +0200

    fix: fixed routes on Navbar

--- web/src/components/UI/Navbar.tsx
+++ web/src/components/UI/Navbar.tsx
@@ -1,14 +1,15 @@
 
 // type Props = {}
 // function Navbar({}: Props) {
-  import icon from "/src/assets/bosque.png"
+  import { Link, NavLink } from "react-router-dom"
+import icon from "/src/assets/bosque.png"
   function Navbar() {
   return (
     <nav className="navbar fixed-top" style={{backgroundColor: "#58C3BB"}}>
       <div className="container-fluid">
         <div className="mx-auto d-flex gap-5">
           <img src={icon} style={{maxHeight: "50px", filter: "drop-shadow(3px 3px 5px rgba(101,62,19,0.8))"}}></img>
-          <a className="navbar-brand fs-2 fw-bolder" href="/" style={{color: "#5A3810", filter: "drop-shadow(3px 3px 5px rgba(101,62,19,0.8))"}}>A casiña da árbore</a>
+          <Link className="navbar-brand fs-2 fw-bolder" to="/" style={{color: "#5A3810", filter: "drop-shadow(3px 3px 5px rgba(101,62,19,0.8))"}}>A casiña da árbore</Link>
         </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span className="navbar-toggler-icon"></span>
@@ -21,16 +22,16 @@
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li className="nav-item">
-                <a className="nav-link fs-3 fw-bolder " aria-current="page" href="/">Inicio</a>
+                <NavLink className={({ isActive }) => isActive ? 'nav-link active fs-3 fw-bolder' : 'nav-link fs-3 fw-bold'} aria-current="page" to="/">Inicio</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link fs-3 fw-bolder " href="/conocenos">Coñécenos</a>
+                <NavLink className={({ isActive }) => isActive ? 'nav-link active fs-3 fw-bolder' : 'nav-link fs-3 fw-bold'} to="/conocenos">Coñécenos</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link fs-3 fw-bolder " href="/posts">Posts</a>
+                <NavLink className={({ isActive }) => isActive ? 'nav-link active fs-3 fw-bolder' : 'nav-link fs-3 fw-bold'} to="/posts">Posts</NavLink>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link fs-3 fw-bolder  dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {/* <li className="nav-item dropdown">
+                <a className="nav-link fs-3 fw-bolder  dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Máis accións
                 </a>
                 <ul className="dropdown-menu">
@@ -41,7 +42,7 @@
                   </li>
                   <li><a className="dropdown-item" href="#">Something else here</a></li>
                 </ul>
-              </li>
+              </li> */}
             </ul>
             {/* <form className="d-flex mt-3" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />

33f028afb4af0e54089d89bd93a90c46d7804dac
Iván <ivantbd87@gmail.com>
2023-06-11 12:43:45 +0200

    auto scrollup on view change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import "./sass/style.scss"
 import PostsScreen from './components/screens/PostsScreen'
 import MainScreen from './components/screens/MainScreen'
 import AboutUs from './components/screens/AboutUs'
 import PostDetailScreen from './components/screens/PostDetailScreen'
 import CreatePost from "./components/screens/CreatePost"
+import { useEffect } from "react"
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    console.log("path", pathname)
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 function App() {
   return (
    <>
+    <ScrollToTop />
     <Routes >
       <Route path='/' element={<MainScreen />}/>
       <Route path='/conocenos' element={<AboutUs />}/>
